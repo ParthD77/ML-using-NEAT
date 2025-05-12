861f51aeca7c70cd14122565ff569b5d0ef8cf71
Parth Dhroovji <parth.dhroovji@gmail.com>
2025-05-12 02:26:06 -0400

    started a main file for running,has agents and fitness etc.(user creates seperate file for use in to_use)

---
+++ main.py
@@ -0,0 +1,62 @@
+from network import Network
+from typing import Tuple, List, Dict
+import math
+import copy
+
+"""
+Potential  features:
+    Activation function????
+    Adjust the % of agents that surive
+"""
+
+class main():
+    agents: List[Network]
+
+
+    def __init__(self, input_size: int = 1, output_size: int = 1, count: int = 10) -> None:
+        self.agents = []
+        for _ in range(len(count)):
+            self.agents.append(Network(input_size, output_size))
+    
+
+    def run_agents(self, input: List[int]) -> List[float]:
+        output = []
+        for network in self.agents:
+            network.process_network(input)
+            output = network.get_output()
+        return output
+
+
+    def rank_fitness(self):
+        # helper callable
+        def get_network_score(network: Network) -> int:
+            return network.score
+        
+        self.agents = sorted(self.agents, key=get_network_score, reverse=True)
+    
+
+    def new_generation(self) -> None:
+        # save 10% of allagents and mutate that 10% to fill the 90% of slots
+        # mutate each agent 9 times and add it
+        spots = len(self.agents)
+        good_agents = self.agents[0 : math.ceil(spots*0.1)]
+        self.agents = good_agents
+
+        for agent in good_agents:
+            for _ in range(9):
+                agent_copy = copy.deepcopy(agent)
+                self.agents.append(agent_copy.mutate_network())
+
+    
+    def get_fitness(self):
+        """
+        Unimplemented Method
+        """
+        pass
+    
+
+
+
+
+if __name__ == "__main__":
+    pass
